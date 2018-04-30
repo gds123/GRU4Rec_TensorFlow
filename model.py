d3d8132b38e91de06584264292157c3dd9427610
guowang <85333103@qq.com>
2018-04-30 10:16:30 +0800

    add prepreo

--- model.py
+++ model.py
@@ -66,6 +66,7 @@ class GRU4Rec:
         else:
             raise NotImplementedError
 
+        # build & save config
         self.checkpoint_dir = args.checkpoint_dir
         if not os.path.isdir(self.checkpoint_dir):
             raise Exception("[!] Checkpoint Dir not found")
@@ -79,6 +80,7 @@ class GRU4Rec:
 
         # use self.predict_state to hold hidden states during prediction. 
         self.predict_state = [np.zeros([self.batch_size, self.rnn_size], dtype=np.float32) for _ in xrange(self.layers)]
+        # restore
         ckpt = tf.train.get_checkpoint_state(self.checkpoint_dir)
         if ckpt and ckpt.model_checkpoint_path:
             self.saver.restore(sess, '{}/gru-model-{}'.format(self.checkpoint_dir, args.test_model))
@@ -178,115 +180,112 @@ class GRU4Rec:
             capped_gvs = gvs
         self.train_op = optimizer.apply_gradients(capped_gvs, global_step=self.global_step)
 
+    def init(self, data):
+        data.sort([self.session_key, self.time_key], inplace=True)
+        offset_sessions = np.zeros(data[self.session_key].nunique() + 1, dtype=np.int32)
+        offset_sessions[1:] = data.groupby(self.session_key).size().cumsum()
+        return offset_sessions
 
-def init(self, data):
-    data.sort([self.session_key, self.time_key], inplace=True)
-    offset_sessions = np.zeros(data[self.session_key].nunique() + 1, dtype=np.int32)
-    offset_sessions[1:] = data.groupby(self.session_key).size().cumsum()
-    return offset_sessions
+    def fit(self, data):
+        self.error_during_train = False
+        itemids = data[self.item_key].unique()
+        self.n_items = len(itemids)
+        self.itemidmap = pd.Series(data=np.arange(self.n_items), index=itemids)
+        data = pd.merge(data, pd.DataFrame({self.item_key: itemids, 'ItemIdx': self.itemidmap[itemids].values}),
+                        on=self.item_key, how='inner')
+        offset_sessions = self.init(data)
+        print('fitting model...')
+        for epoch in xrange(self.n_epochs):
+            epoch_cost = []
+            state = [np.zeros([self.batch_size, self.rnn_size], dtype=np.float32) for _ in xrange(self.layers)]
+            session_idx_arr = np.arange(len(offset_sessions) - 1)
+            iters = np.arange(self.batch_size)
+            maxiter = iters.max()
+            start = offset_sessions[session_idx_arr[iters]]
+            end = offset_sessions[session_idx_arr[iters] + 1]
+            finished = False
+            while not finished:
+                minlen = (end - start).min()
+                out_idx = data.ItemIdx.values[start]
+                for i in range(minlen - 1):
+                    in_idx = out_idx
+                    out_idx = data.ItemIdx.values[start + i + 1]
+                    # prepare inputs, targeted outputs and hidden states
+                    fetches = [self.cost, self.final_state, self.global_step, self.lr, self.train_op]
+                    feed_dict = {self.X: in_idx, self.Y: out_idx}
+                    for j in xrange(self.layers):
+                        feed_dict[self.state[j]] = state[j]
 
+                    cost, state, step, lr, _ = self.sess.run(fetches, feed_dict)
+                    epoch_cost.append(cost)
+                    if np.isnan(cost):
+                        print(str(epoch) + ':Nan error!')
+                        self.error_during_train = True
+                        return
+                    if step == 1 or step % self.decay_steps == 0:
+                        avgc = np.mean(epoch_cost)
+                        print('Epoch {}\tStep {}\tlr: {:.6f}\tloss: {:.6f}'.format(epoch, step, lr, avgc))
+                start = start + minlen - 1
+                mask = np.arange(len(iters))[(end - start) <= 1]
+                for idx in mask:
+                    maxiter += 1
+                    if maxiter >= len(offset_sessions) - 1:
+                        finished = True
+                        break
+                    iters[idx] = maxiter
+                    start[idx] = offset_sessions[session_idx_arr[maxiter]]
+                    end[idx] = offset_sessions[session_idx_arr[maxiter] + 1]
+                if len(mask) and self.reset_after_session:
+                    for i in xrange(self.layers):
+                        state[i][mask] = 0
 
-def fit(self, data):
-    self.error_during_train = False
-    itemids = data[self.item_key].unique()
-    self.n_items = len(itemids)
-    self.itemidmap = pd.Series(data=np.arange(self.n_items), index=itemids)
-    data = pd.merge(data, pd.DataFrame({self.item_key: itemids, 'ItemIdx': self.itemidmap[itemids].values}),
-                    on=self.item_key, how='inner')
-    offset_sessions = self.init(data)
-    print('fitting model...')
-    for epoch in xrange(self.n_epochs):
-        epoch_cost = []
-        state = [np.zeros([self.batch_size, self.rnn_size], dtype=np.float32) for _ in xrange(self.layers)]
-        session_idx_arr = np.arange(len(offset_sessions) - 1)
-        iters = np.arange(self.batch_size)
-        maxiter = iters.max()
-        start = offset_sessions[session_idx_arr[iters]]
-        end = offset_sessions[session_idx_arr[iters] + 1]
-        finished = False
-        while not finished:
-            minlen = (end - start).min()
-            out_idx = data.ItemIdx.values[start]
-            for i in range(minlen - 1):
-                in_idx = out_idx
-                out_idx = data.ItemIdx.values[start + i + 1]
-                # prepare inputs, targeted outputs and hidden states
-                fetches = [self.cost, self.final_state, self.global_step, self.lr, self.train_op]
-                feed_dict = {self.X: in_idx, self.Y: out_idx}
-                for j in xrange(self.layers):
-                    feed_dict[self.state[j]] = state[j]
+            avgc = np.mean(epoch_cost)
+            if np.isnan(avgc):
+                print('Epoch {}: Nan error!'.format(epoch, avgc))
+                self.error_during_train = True
+                return
+            self.saver.save(self.sess, '{}/gru-model'.format(self.checkpoint_dir), global_step=epoch)
 
-                cost, state, step, lr, _ = self.sess.run(fetches, feed_dict)
-                epoch_cost.append(cost)
-                if np.isnan(cost):
-                    print(str(epoch) + ':Nan error!')
-                    self.error_during_train = True
-                    return
-                if step == 1 or step % self.decay_steps == 0:
-                    avgc = np.mean(epoch_cost)
-                    print('Epoch {}\tStep {}\tlr: {:.6f}\tloss: {:.6f}'.format(epoch, step, lr, avgc))
-            start = start + minlen - 1
-            mask = np.arange(len(iters))[(end - start) <= 1]
-            for idx in mask:
-                maxiter += 1
-                if maxiter >= len(offset_sessions) - 1:
-                    finished = True
-                    break
-                iters[idx] = maxiter
-                start[idx] = offset_sessions[session_idx_arr[maxiter]]
-                end[idx] = offset_sessions[session_idx_arr[maxiter] + 1]
-            if len(mask) and self.reset_after_session:
-                for i in xrange(self.layers):
-                    state[i][mask] = 0
+    def predict_next_batch(self, session_ids, input_item_ids, itemidmap, batch=50):
+        '''
+        Gives predicton scores for a selected set of items. Can be used in batch mode to predict for multiple independent events (i.e. events of different sessions) at once and thus speed up evaluation.
 
-        avgc = np.mean(epoch_cost)
-        if np.isnan(avgc):
-            print('Epoch {}: Nan error!'.format(epoch, avgc))
-            self.error_during_train = True
-            return
-        self.saver.save(self.sess, '{}/gru-model'.format(self.checkpoint_dir), global_step=epoch)
+        If the session ID at a given coordinate of the session_ids parameter remains the same during subsequent calls of the function, the corresponding hidden state of the network will be kept intact (i.e. that's how one can predict an item to a session).
+        If it changes, the hidden state of the network is reset to zeros.
 
+        Parameters
+        --------
+        session_ids : 1D array
+            Contains the session IDs of the events of the batch. Its length must equal to the prediction batch size (batch param).
+        input_item_ids : 1D array
+            Contains the item IDs of the events of the batch. Every item ID must be must be in the training data of the network. Its length must equal to the prediction batch size (batch param).
+        batch : int
+            Prediction batch size.
 
-def predict_next_batch(self, session_ids, input_item_ids, itemidmap, batch=50):
-    '''
-    Gives predicton scores for a selected set of items. Can be used in batch mode to predict for multiple independent events (i.e. events of different sessions) at once and thus speed up evaluation.
+        Returns
+        --------
+        out : pandas.DataFrame
+            Prediction scores for selected items for every event of the batch.
+            Columns: events of the batch; rows: items. Rows are indexed by the item IDs.
 
-    If the session ID at a given coordinate of the session_ids parameter remains the same during subsequent calls of the function, the corresponding hidden state of the network will be kept intact (i.e. that's how one can predict an item to a session).
-    If it changes, the hidden state of the network is reset to zeros.
+        '''
+        if batch != self.batch_size:
+            raise Exception('Predict batch size({}) must match train batch size({})'.format(batch, self.batch_size))
+        if not self.predict:
+            self.current_session = np.ones(batch) * -1
+            self.predict = True
 
-    Parameters
-    --------
-    session_ids : 1D array
-        Contains the session IDs of the events of the batch. Its length must equal to the prediction batch size (batch param).
-    input_item_ids : 1D array
-        Contains the item IDs of the events of the batch. Every item ID must be must be in the training data of the network. Its length must equal to the prediction batch size (batch param).
-    batch : int
-        Prediction batch size.
+        session_change = np.arange(batch)[session_ids != self.current_session]
+        if len(session_change) > 0:  # change internal states with session changes
+            for i in xrange(self.layers):
+                self.predict_state[i][session_change] = 0.0
+            self.current_session = session_ids.copy()
 
-    Returns
-    --------
-    out : pandas.DataFrame
-        Prediction scores for selected items for every event of the batch.
-        Columns: events of the batch; rows: items. Rows are indexed by the item IDs.
-
-    '''
-    if batch != self.batch_size:
-        raise Exception('Predict batch size({}) must match train batch size({})'.format(batch, self.batch_size))
-    if not self.predict:
-        self.current_session = np.ones(batch) * -1
-        self.predict = True
-
-    session_change = np.arange(batch)[session_ids != self.current_session]
-    if len(session_change) > 0:  # change internal states with session changes
+        in_idxs = itemidmap[input_item_ids]
+        fetches = [self.yhat, self.final_state]
+        feed_dict = {self.X: in_idxs}
         for i in xrange(self.layers):
-            self.predict_state[i][session_change] = 0.0
-        self.current_session = session_ids.copy()
-
-    in_idxs = itemidmap[input_item_ids]
-    fetches = [self.yhat, self.final_state]
-    feed_dict = {self.X: in_idxs}
-    for i in xrange(self.layers):
-        feed_dict[self.state[i]] = self.predict_state[i]
-    preds, self.predict_state = self.sess.run(fetches, feed_dict)
-    preds = np.asarray(preds).T
-    return pd.DataFrame(data=preds, index=itemidmap.index)
+            feed_dict[self.state[i]] = self.predict_state[i]
+        preds, self.predict_state = self.sess.run(fetches, feed_dict)
+        preds = np.asarray(preds).T
+        return pd.DataFrame(data=preds, index=itemidmap.index)
